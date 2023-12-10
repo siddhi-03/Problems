b8c3f1ba5c3a43ece537ca8def452bfec67e833c
Siddhi Taori <70755953+siddhi-03@users.noreply.github.com>
2023-12-10 20:20:09 +0530

    Create jumpingOnTheCloudsRevisited.java

---
+++ jumpingOnTheCloudsRevisited.java
@@ -0,0 +1,69 @@
+//Hackerrank problem :- https://www.hackerrank.com/contests/mountblue-technologies/challenges/jumping-on-the-clouds-revisited
+//Jumping on the Clouds: Revisited
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k) {
+//  int index = 0;
+//         int e= 100;
+//         for(int i =0;i<c.length;i++){
+//             index = (index + k)% c.length;
+//             if(c[index] == 1) e = e -3;
+//             else e = e-1;
+//             if(index == 0) break;
+            
+//         }
+//        return e;
+        
+        //We can use do while loop here instead of using two diff index
+        int e = 100;
+        int n = c.length;
+        int i =0;
+        do{
+             i = (i+k)%n;
+            e -=c[i]==1?3:1;
+            
+        }while(i!=0);
+     return e;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
